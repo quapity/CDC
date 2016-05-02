2c5d1da6175fd38ba0de50376fd6bbe9b7e3da08
quapity <quapity@gmail.com>
2016-05-02 10:09:41 -0600

    testing to run as module

---
+++ __init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------------------------
+#   Author: Lisa Linville
+#------------------------------------------------------------------------------
+"""
+LDK: A Python toolbox for earthquake detections
+==================================================================
+"""
+# General imports
+import os
+import sys
+
+# LDK imports
+import detection_function
+import Util
+
